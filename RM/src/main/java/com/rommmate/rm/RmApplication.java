80e487041197005fa52082fcfe40d47dd71bfd66
안수민 <20201052@vision.hoseo.edu>
2023-10-31 15:43:50 +0900

    setting

---
+++ RM/src/main/java/com/rommmate/rm/RmApplication.java
@@ -0,0 +1,13 @@
+package com.rommmate.rm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RmApplication.class, args);
+    }
+
+}
---
+++ RM/src/test/java/com/rommmate/rm/RmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rommmate.rm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
